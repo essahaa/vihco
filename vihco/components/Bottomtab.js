9d926ef25825c2c5b689f5c4dc2336eceb36936b
pinjaa <91983754+pinjaa@users.noreply.github.com>
2023-03-29 12:51:33 +0300

    login ja register lisätty naviin koodauksen ajaksi

--- vihco/components/Bottomtab.js
+++ vihco/components/Bottomtab.js
@@ -5,6 +5,8 @@ import Account from './Account';
 import Games from './Games';
 import Sheets from './Sheets';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'; 
+import Login from './Login';
+import Register from './Register';
 
 
 const Tab = createBottomTabNavigator();
@@ -17,6 +19,8 @@ export default Bottomtab = () => {
       <Tab.Screen name="Games"  component={Games} options={{tabBarIcon:()=> <MaterialCommunityIcons name="cards-playing" size={24} color="black" />}}/>
       <Tab.Screen name="Sheets"  component={Sheets} options={{tabBarIcon:()=> <MaterialCommunityIcons name="file-document-multiple-outline" size={24} color="black" />}}/>
       <Tab.Screen name="Account"  component={Account} options={{tabBarIcon:()=> <MaterialIcons name="account-circle" size={24} color="black" />}}/>
+      <Tab.Screen name="Login"  component={Login} options={{tabBarIcon:()=> <MaterialIcons name="account-circle" size={24} color="black" />}}/>
+      <Tab.Screen name="Register"  component={Register} options={{tabBarIcon:()=> <MaterialIcons name="account-circle" size={24} color="black" />}}/>
     </Tab.Navigator>
     </NavigationContainer>
   );
