fd9a8e92a8c022b2b4748e853debd129c630d7eb
paulihaarnio <pauli.haarnio@gmail.com>
2023-03-29 12:58:16 +0300

    push

--- vihco/firebase/Config.js
+++ vihco/firebase/Config.js
@@ -1,15 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = ({
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-});
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCeu_X_bjM7cY_dUNL0E2H2RIhVoiSY8fk",
+  authDomain: "vihco-91d29.firebaseapp.com",
+  projectId: "vihco-91d29",
+  storageBucket: "vihco-91d29.appspot.com",
+  messagingSenderId: "878230632373",
+  appId: "1:878230632373:web:448753fdcf49d331a58bfc",
+  measurementId: "G-8L64HHZFTS"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const TODOS_REF = 'todos';
+export const USERS_REF = '/users/';
+
+const analytics = getAnalytics(app);
