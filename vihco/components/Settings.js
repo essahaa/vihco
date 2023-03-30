c67c80839d521795bfc4cf873a62333d6de9d5e2
pinjaa <91983754+pinjaa@users.noreply.github.com>
2023-03-30 14:55:23 +0300

    settings sivun alku

--- vihco/components/Settings.js
+++ vihco/components/Settings.js
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View, Button, Pressable } from 'react-native';
+import { logOut } from './Auth';
 
 
 export default function Settings() {
   return (
     <View >
       <Text>Settings</Text>
+      <Pressable >
+                <Button 
+                    title="Logout"
+                    onPress={logOut}
+                />
+            </Pressable>
     </View>
   );
 }
