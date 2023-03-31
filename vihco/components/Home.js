415355d9e36e568454bfd7ed052ffedcabd8d4ca
pinjaa <91983754+pinjaa@users.noreply.github.com>
2023-03-31 15:33:15 +0300

    hakee käyttäjän nimen etusivulle,ei toimi kunnolla

--- vihco/components/Home.js
+++ vihco/components/Home.js
@@ -1,10 +1,37 @@
 import { Text, View } from 'react-native';
 import styles from '../styles/style';
+import { useEffect, useState } from 'react';
+import { doc, getDoc } from "firebase/firestore";
+import { db, USERS_REF } from '../firebase/Config';
 
-export default function Home() {
+export default function Home({navigation, route}) {
+
+  const [username, setUsername] = useState('')
+  const [userUid, setUserUid] = useState('')
+
+  useEffect(() => {
+    setUserUid(route.params.userUid)
+    getUserData()
+  }, [])
+
+  const getUserData = async () => {
+    const docRef = doc(db, USERS_REF, userUid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      const data = docSnap.data()
+      const username = data.name
+      setUsername(username)
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home</Text>
+      <Text style={styles.text}>Welcome {username}</Text>
     </View>
   );
 }
