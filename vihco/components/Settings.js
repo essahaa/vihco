7dab7700b3b3978313ba8045401c94563378d870
Essa Haapanen <n1haes00@students.oamk.fi>
2023-03-31 13:47:10 +0300

    dropdown

--- vihco/components/Settings.js
+++ vihco/components/Settings.js
@@ -1,7 +1,9 @@
+import React, { useState } from 'react';
 import styles from '../styles/style';
 import {  Text, View, Button, Pressable } from 'react-native';
 import { logOut } from './Auth';
 import Logo from './Logo';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function Settings({navigation}) {
 
@@ -10,10 +12,33 @@ export default function Settings({navigation}) {
     navigation.navigate('Login');
   }
 
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(['italy', 'spain', 'barcelona', 'finland']);
+  const [items, setItems] = useState([
+    {label: 'Spain', value: 'spain'},
+    {label: 'Madrid', value: 'madrid', parent: 'spain'},
+    {label: 'Barcelona', value: 'barcelona', parent: 'spain'},
+
+    {label: 'Italy', value: 'italy'},
+    {label: 'Rome', value: 'rome', parent: 'italy'},
+
+    {label: 'Finland', value: 'finland'}
+  ]);
+
   return (
     <View style={styles.overlay}>
       <Logo />
       <Text style={styles.text}>Settings</Text>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        theme="DARK"
+        multiple={true}
+      />
       <Pressable
           onPress={() => handlePress()}
           style={styles.buttonLogout}
