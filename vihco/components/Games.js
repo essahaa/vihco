283d1718b7dbfc384b0fe2786c495cf294ed112b
booennm <booennm@gmail.com>
2023-03-31 14:14:18 +0300

    gets games from db

--- vihco/components/Games.js
+++ vihco/components/Games.js
@@ -1,12 +1,34 @@
-import { Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Text, View, ScrollView } from 'react-native';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db, GAMES_REF } from '../firebase/Config';
 import Header from './Header';
 import styles from '../styles/style';
 
 export default function Games() {
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    const q = query(collection(db, GAMES_REF), orderBy("id"))
+    onSnapshot(q, (querySnapshot) => {
+      setGames(querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      })));
+    });
+    console.log(games);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Header />
-      <Text style={styles.text}>Games</Text>
+      <ScrollView>
+        {games.map((key, i) => (
+              <Text style={styles.text} key={i}>{games[i].name}</Text>
+        ))
+        }
+      </ScrollView>
+      
     </View>
   );
 }
