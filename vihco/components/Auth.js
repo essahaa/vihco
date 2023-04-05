7c4020ec8840d87aa75736c5a2ed72d4ecb21d4a
pinjaa <91983754+pinjaa@users.noreply.github.com>
2023-04-05 12:30:26 +0300

    korjattu uudestaan

--- vihco/components/Auth.js
+++ vihco/components/Auth.js
@@ -10,12 +10,19 @@ export const signUp = async (username, email, password) => {
         .then((userCredential) => { 
             if(userCredential.user.uid !== "") {
                 try {
-                    const usersCollection = collection(db, USERS_REF);
-                    const usersDocumentData =  {
-                      name: username, 
-                      email: userCredential.user.email
-                    };
-                    const usersDocRef = doc(usersCollection, userCredential.user.uid);
+                    // const usersCollection = collection(db, USERS_REF);
+                    // const usersDocumentData =  {
+                    //   name: username, 
+                    //   email: userCredential.user.email
+                    // };
+                    // const usersDocRef = doc(usersCollection, userCredential.user.uid);
+                    // setDoc(usersDocRef, usersDocumentData);
+                    // console.log("Document written with ID: ", userCredential.user.uid);
+                    const usersDocRef = doc(db, USERS_REF, userCredential.user.uid)
+                    const usersDocumentData = {
+                        name: username,
+                        email: userCredential.user.email
+                    }
                     setDoc(usersDocRef, usersDocumentData);
                     console.log("Document written with ID: ", userCredential.user.uid);
                   } catch (e) {
