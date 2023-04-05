1d57766dcc2807412595a794ca57695bc825d588
pinjaa <91983754+pinjaa@users.noreply.github.com>
2023-04-05 11:59:21 +0300

    groups sivun pohja tehty

---
+++ vihco/components/Groups.js
@@ -0,0 +1,11 @@
+import { Text, View, Pressable, Button } from 'react-native';
+import styles from '../styles/style';
+
+export default function Groups({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Groups</Text>
+      
+    </View>
+  );
+}
