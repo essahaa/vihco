7af5709966e6736a0454d59b0b133ce85516a546
pinjaa <91983754+pinjaa@users.noreply.github.com>
2023-04-14 12:11:49 +0300

    ryhmän lisäys toimii, admin homma myös

--- vihco/components/Groups.js
+++ vihco/components/Groups.js
@@ -5,22 +5,50 @@ import { collection, onSnapshot, orderBy, query, addDoc, where, getDocs, data, g
 import styles from '../styles/style';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Header2 from './Header2';
+import { getAuth } from 'firebase/auth';
 
 export default function Groups({navigation}) {
     const [groupname, setGroupname] = useState('')
     const [groupId, setGroupId] = useState('')
-    const [groups, setGroups] = useState([])
     const [playerEmail, setPlayerEmail] = useState('');
     const [playerId, setPlayerId] = useState('')
     const [playerName, setPlayerName] = useState('')
     const [players, setPlayers] = useState([]);
     const [group, setGroup] = useState([])
 
+    const auth = getAuth()
+
+    useEffect(() => {
+      getPlayers()
+    }, [])
+
+
+    // HAE JOLLAIN KEINOLLA TIETYN RYHMÄN PELAAJAT DATABASESTA
+    //TÄMÄ EI TOIMI
+    const getPlayers = async () => {
+      const groupQ = query(collection(db, GROUPS_REF + "/" + groupId + "/users"), where("name", "==", groupname));
+      const groupQuerySnapShot = await getDocs(groupQ);
+      setPlayers(groupQuerySnapShot)
+    }
+    
     
     const addPlayer = async () => {
       //hakee syötettyä sähköpostiosoitetta vastaavan dokumentin
       const q = query(collection(db, USERS_REF), where("email", "==", playerEmail));
       const querySnapshot = await getDocs(q);
+
+      const groupQ = query(collection(db, GROUPS_REF), where("name", "==", groupname));
+      const groupQuerySnapShot = await getDocs(groupQ);
+
+      groupQuerySnapShot.forEach((doc) => {
+        const data2 = doc.data()
+        setGroup(groupQuerySnapShot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        })));
+        console.log("Group data: " + data2.name + " => " + doc.id)
+      });
+      console.log("group id: " + groupId)
 
       if(querySnapshot.empty) {
         console.log("No such user found!")
@@ -32,40 +60,42 @@ export default function Groups({navigation}) {
           setPlayerName(data.name)
           console.log("Username data: " + data.name + " => " + doc.id)
         });
-      }
 
-      const groupQ = query(collection(db, GROUPS_REF), where("name", "==", groupname));
-      const groupQuerySnapShot = await getDocs(groupQ);
-
-      groupQuerySnapShot.forEach((doc) => {
-        const data2 = doc.data()
-        //setGroupId(doc.id)
-        setGroup(groupQuerySnapShot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        })));
-        console.log("Group data: " + data2.name + " => " + doc.id)
-      });
-      console.log("group id: " + groupId)
-      const usersDocRef = doc(db, GROUPS_REF + "/" + groupId + "/users", playerId)
+        const usersDocRef = doc(db, GROUPS_REF + "/" + groupId + "/users", playerId)
     
-      await setDoc((usersDocRef), {
-        name: playerName,
-      })
+        await setDoc((usersDocRef), {
+          name: playerName,
+          admin: false
+        })
+      }
     }
 
     const addNewGroup = async () => {
-        try {
-            if(groupname.trim() !== "") {
-              const groupAdded = await addDoc(collection(db, GROUPS_REF), {
-                name: groupname
-              });
-              console.log("group added with id: " + groupAdded.id);
-              setGroupId(groupAdded.id)
+      try {
+          if(groupname.trim() !== "") {
+            const groupAdded = await addDoc(collection(db, GROUPS_REF), {
+              name: groupname
+            });
+            console.log("group added with id: " + groupAdded.id);
+            setGroupId(groupAdded.id);
+
+            //lisää ryhmän luoneen käyttäjän ryhmään ja ryhmän adminiksi
+            const currentUserId = auth.currentUser.uid;
+            const docRef = doc(db, USERS_REF, currentUserId);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+              //console.log("Document data:", docSnap.data());
+              const data = docSnap.data()
+              const usersDocRef = doc(db, GROUPS_REF + "/" + groupAdded.id + "/users", currentUserId);
+              await setDoc((usersDocRef), {
+                name: data.name,
+                admin: true
+              })
             }
-          }catch (error) {
-            console.log(error.message);
           }
+        }catch (error) {
+          console.log(error.message);
+        }
     }
 
   return (
@@ -74,6 +104,7 @@ export default function Groups({navigation}) {
         <ScrollView contentContainerStyle={styles.scrollview}
         style={{marginBottom: 20}}>
         <View>
+            <Text style={styles.text}>Groupname: {groupname}</Text>
             <Text style={styles.text}>Add new group</Text>
             <TextInput 
                 style={styles.textInput}
@@ -112,7 +143,7 @@ export default function Groups({navigation}) {
         <View >
         <Text style={styles.title}>PLAYERS</Text>
         
-          {groups.map((key,i) => (
+          {players.map((key,i) => (
             <View key={i} style={[styles.gameButton, {height: 120}]}>
               <Text style={styles.gameText} >{groups[i].name} + {groups[i].id}</Text>
               
