b6ac5a36fd990dede96600e3ef2a3a6a71f2a272
booennm <booennm@gmail.com>
2023-04-18 15:36:17 +0300

    game deleting works

--- vihco/components/GameSettings.js
+++ vihco/components/GameSettings.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { View, Text, Pressable, TextInput, Alert } from 'react-native';
-import { updateDoc, doc } from 'firebase/firestore';
+import { updateDoc, doc, query, collection, onSnapshot, deleteDoc } from 'firebase/firestore';
 import styles from '../styles/style';
 import Header from './Header';
 import { db, GAMES_REF } from '../firebase/Config';
@@ -9,6 +9,7 @@ export default GameSettings = ({navigation, route}) => {
     const [name, setName] = useState('');
     const [id, setId] = useState('');
     const [newName, setNewName] = useState('');
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         if( name === '' && route.params?.gameName ) {
@@ -37,6 +38,57 @@ export default GameSettings = ({navigation, route}) => {
         }
     }
     
+    /* const resetScores = async () => {
+        try {
+            const q = query(collection(db, "/games/" + id + "/users"))
+            onSnapshot(q, (querySnapshot) => {
+                querySnapshot.docs.map((user) => ({
+                    updateDoc(doc(db, "/games/" + id + "/users", user.id), {
+                        win: 0,
+                        loss: 0
+                    })
+            }))}).then(
+            Alert.alert('Scores reset',
+            "Success", [
+                {text: 'OK', onPress: () => navigation.navigate("Games")},
+            ]))
+        }catch (error) {
+            console.log(error.message);
+        }
+    } */
+
+    const deleteGame = async () => {
+        try {
+            const q = query(collection(db, "/games/" + id + "/users"))
+            onSnapshot(q, (querySnapshot) => {
+                querySnapshot.docs.map(function(user) {
+                    deleteDoc(doc(db, "/games/" + id + "/users", user.id));
+                });
+            }).then(
+                deleteDoc(doc(db, GAMES_REF, id)).then(
+                    Alert.alert('Game deleted',
+                    "", [
+                        {text: 'OK', onPress: () => navigation.navigate("Games")},
+                    ])
+                )
+            )
+        }catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const handleDeletePress = () => {
+        Alert.alert('Delete game?',
+        `Are you sure you want to delete game "` + name + `"?` +
+        `\nThis action cannot be undone.`, [
+                {
+                text: 'CANCEL',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+                },
+                {text: 'DELETE', onPress: () => deleteGame()},
+            ])
+    }
     
     return (
         <View style={styles.container}>
@@ -54,19 +106,19 @@ export default GameSettings = ({navigation, route}) => {
                     placeholderTextColor='#4E9BB0'
                 />
                 <Pressable
-                    onPress={changeName}
+                    onPress={() => changeName()}
                     style={styles.buttonPrimary}
                     >
                     <Text style={[styles.buttonText, {fontSize: 16, textAlign: 'center'}]}>SAVE NEW NAME</Text>
                 </Pressable>
                 <Pressable
-                    //onPress={addPlayer}
+                    onPress={() => resetScores()}
                     style={[styles.buttonSettings, {marginTop: 60}]}
                     >
                     <Text style={[styles.buttonTextSettings, {paddingVertical: 5}]}>RESET SCORES</Text>
                 </Pressable>
                 <Pressable
-                    //onPress={addPlayer}
+                    onPress={handleDeletePress}
                     style={[styles.buttonLogout, {marginTop: 20}]}
                     >
                     <Text style={styles.buttonTextLogout}>DELETE GAME</Text>
